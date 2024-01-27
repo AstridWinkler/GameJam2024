625c4f41aeb25cc67344f47a2423e472ff38fcc6
katastorm <astrid17.winkler@gmail.com>
2024-01-27 17:34:05 +0100

    add lerp to mob body part

--- Assets/Scripts/Astrid/EnemyTrajectory.cs
+++ Assets/Scripts/Astrid/EnemyTrajectory.cs
@@ -6,17 +6,35 @@ using UnityEngine.UI;
 public class EnemyTrajectory : MonoBehaviour
 {
     public Transform objectToFollow;
-    //public Vector3 offset;
+    public Transform eyes;
+    public Transform tail;
+    public Transform tail2;
+
+    float eyeDist = 0.5f;
+    float tailDist = 1.3f;
+    float tail2Dist = 0.8f;
+    Vector3 distToBody;
 
     public float MoveSpeed;
     public float MaxDist;
     public float MinDist;
-    public Vector3 dist;
 
     // Start is called before the first frame update
     void Start()
     {
         objectToFollow = GameManager.Gameplay.CurrentPlayer.transform;
+    }
+
+    private void directionDisplacement(Vector3 fromObject, Vector3 toObject)
+    {
+
+        var targetDistToBody = (toObject - fromObject).normalized;
+        distToBody = Vector3.Lerp(distToBody, targetDistToBody, 3.5f * Time.deltaTime);
+
+        eyes.localPosition = distToBody * eyeDist;
+        tail.localPosition = -distToBody * tailDist;
+        tail2.localPosition = -distToBody * tail2Dist;
+
     }
 
     void Update()
@@ -25,18 +43,17 @@ public class EnemyTrajectory : MonoBehaviour
         {
             Start();
         }
-        //transform.LookAt(Player);
 
-        if (Vector3.Distance(transform.position, objectToFollow.position) >= MinDist)
+        if ((Vector3.Distance(transform.position, objectToFollow.position) >= MinDist) && (Vector3.Distance(transform.position, objectToFollow.position) <= MaxDist))
         {
             var step = MoveSpeed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, objectToFollow.position, step);
 
-            //if (Vector3.Distance(transform.position, objectToFollow.position) <= MaxDist)
-            //{
-                //Here Call any function U want Like Shoot at here or something
-            //}
-
+            directionDisplacement(transform.position, objectToFollow.position);
+        }
+        else
+        {
+            directionDisplacement(transform.position, transform.position);
         }
 
     }
