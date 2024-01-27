546c31c09f80c8ba954612f87b30eda3b5d004a1
jfaidide <logiked-t@yahoo.com>
2024-01-27 15:02:06 +0100

    Shimmer Pits V1 + Blood update

---
+++ Assets/Scripts/InGame/ShimmerPit/ShimmerLevelBlock.cs
@@ -0,0 +1,155 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+#if UNITY_EDITOR
+using logiked.source.editor;
+using UnityEditor;
+#endif
+
+
+[ExecuteAlways]
+public class ShimmerLevelBlock : MonoBehaviour
+{
+
+    public static ShimmerLevelBlock Instance { get; private set; }
+
+    /// <summary>
+    /// Déplacement en X de chaque mini-niveau de depth
+    /// </summary>
+    public const float depthOffsetX = 100;
+    /// <summary>
+    /// Déplacement en Y de chaque mini-niveau de depth
+    /// </summary>
+    public const float depthOffsetY = -100;
+
+    public Transform TilemapsParent { get => tilemapsParent; }
+    [SerializeField]
+    Transform tilemapsParent;
+
+    [Tooltip("Nombre de différents puits de shimmers dans cette map")]
+    [SerializeField]
+    [Range(1, 10)]
+    int numberOfShimmerPit = 1;
+
+    [SerializeField]
+    Transform shimmerPitParents;
+    [SerializeField]
+    List<ShimmerPit> shimerPitList = new List<ShimmerPit>();
+
+
+
+#if UNITY_EDITOR
+    [InitializeOnLoadMethod]
+   static void AwakeReload()
+    {
+      Instance = FindObjectOfType<ShimmerLevelBlock>();
+    }
+#endif
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Update()
+    {
+
+    }
+
+
+
+
+
+
+#if UNITY_EDITOR
+
+    public void UpdateChildDepth()
+    {
+        if (shimmerPitParents == null)
+        {
+            shimmerPitParents = new GameObject("ShimmerPits").transform;
+            shimmerPitParents.SetParent(transform);
+            transform.localPosition = Vector3.zero;
+        }
+
+
+        if (shimmerPitParents != null)
+        {
+
+            var existingPits = shimmerPitParents.GetComponentsInChildren<ShimmerPit>(true);
+            List<ShimmerPit> end = new List<ShimmerPit>();
+            int i = 0;
+            for (i = 0; i < numberOfShimmerPit; i++)
+            {
+                if (i < existingPits.Length)
+                    end.Add(existingPits[i]);
+                else
+                    end.Add(CreateNewPit());
+            }
+
+
+            i = 0;
+            foreach (var e in end)
+            {
+                e.transform.position = new Vector3(i * depthOffsetX, 0, 0);
+                e.name = "pit_" + i++;
+                e.gameObject.SetActive(true);
+                e.UpdateShimmerDepth();
+            }
+
+
+            foreach (var e in existingPits)
+                if (!end.Contains(e))
+                {
+                    e.name = "pit_not_used";
+                    e.gameObject.SetActive(false);
+                }
+
+
+
+
+            shimerPitList = end;
+
+        }
+
+
+    }
+
+    ShimmerPit CreateNewPit()
+    {
+        var pit = new GameObject("Pit").AddComponent<ShimmerPit>();
+        pit.transform.SetParent(shimmerPitParents);
+        pit.transform.localPosition = Vector3.zero;
+        return pit;
+    }
+
+
+
+
+
+    [CustomEditor(typeof(ShimmerLevelBlock))]
+    private class ShimmerLevelBlockEditor : Editor
+    {
+        ShimmerLevelBlock t;
+        public override void OnInspectorGUI()
+        {
+            t = target as ShimmerLevelBlock;
+
+            DrawDefaultInspector();
+            if (GUILayout.Button("Update Childs Pits"))
+            {
+                t.UpdateChildDepth();
+                t.SetDirtyNow();
+                serializedObject.ApplyModifiedProperties();
+            }
+
+        }
+
+
+    }
+
+#endif
+
+}
---
+++ Assets/Scripts/InGame/ShimmerPit/ShimmerPit.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using logiked.source.editor;
+using UnityEditor;
+#endif
+public class ShimmerPit : MonoBehaviour
+{
+
+    [Tooltip("Nombre de différents puits de shimmers dans cette map")]
+    [SerializeField]
+    [Range(1,4)]
+    int numberOfDepth = 1;
+
+    [SerializeField]
+    List<ShimmerDepthFloor> shimmerDepthList = new List<ShimmerDepthFloor>();
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+#if UNITY_EDITOR
+
+
+
+
+  public  void UpdateShimmerDepth()
+    {
+
+
+        var existingPits = GetComponentsInChildren<ShimmerDepthFloor>(true);
+        List<ShimmerDepthFloor> end = new List<ShimmerDepthFloor>();
+        int i = 0;
+        for (i = 0; i < numberOfDepth; i++)
+        {
+            if (i < existingPits.Length)
+                end.Add(existingPits[i]);
+            else
+                end.Add(CreateNewDepth());
+        }
+
+
+        i = 0;
+        foreach (var e in end)
+        {
+            e.transform.localPosition = new Vector3(0, (1+i) * ShimmerLevelBlock.depthOffsetY, 0);
+            e.name = "depth_" + i++;
+            e.gameObject.SetActive(true);
+            e.UpdateTilemapClone();
+
+        }
+
+        foreach (var e in existingPits)
+            if (!end.Contains(e))
+            {
+                e.name = "depth_not_used";
+                e.gameObject.SetActive(false);
+
+            }
+
+
+
+        shimmerDepthList = end;
+
+
+
+
+    }
+
+    ShimmerDepthFloor CreateNewDepth()
+    {
+        var pit = new GameObject("Depth").AddComponent<ShimmerDepthFloor>();
+        pit.transform.SetParent(transform);
+        pit.transform.localPosition = Vector3.zero;
+        return pit;
+    }
+
+
+
+
+
+
+
+    [CustomEditor(typeof(ShimmerPit))]
+    private class ShimmerPitEditor : Editor
+    {
+        ShimmerPit shim;
+        public override void OnInspectorGUI()
+        {
+            shim = target as ShimmerPit;
+
+            DrawDefaultInspector();
+            if (GUILayout.Button("Update Childs Depths"))
+            {
+                shim.UpdateShimmerDepth();
+                shim.SetDirtyNow();
+                serializedObject.ApplyModifiedProperties();
+            }
+
+        }
+
+    
+    }
+
+#endif
+
+
+}
